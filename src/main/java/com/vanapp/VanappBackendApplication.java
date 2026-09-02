a21d6cda1318652637e471f777d3e651f83692d5
Pethersonzada <pethersonzada@gmail.com>
2026-09-02 10:40:34 -0300

    Tornando o carregamento do Dotenv opcional

--- src/main/java/com/vanapp/VanappBackendApplication.java
+++ src/main/java/com/vanapp/VanappBackendApplication.java
@@ -7,10 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VanappBackendApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        try {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            if (dotenv.get("DATABASE_URL") != null) {
+                System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+                System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+                System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+            }
+        } catch (Exception e) {
+            // Ignora se o .env não existir (como no Render)
+        }
+        
         SpringApplication.run(VanappBackendApplication.class, args);
     }
 }
